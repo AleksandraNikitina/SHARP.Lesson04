985d90c6219008fdca49481007716b5a63b312dd
Aleksandra <asandrabelka@gmail,com>
2024-11-07 14:38:55 +0200

    Add Lecture Tasks

---
+++ Lecture1Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Input from console a string with letters and digits.
+// Create a new string with letters from the sourse string.
+
+string GetLettersFromString(string s)
+{
+    string letters ="";
+    foreach(char e in s)
+    {
+        if (char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+Console.Write("Enter string with letters and digits: ");
+string str = Console.ReadLine();
+string result = GetLettersFromString(str);
+Console.WriteLine(result);
