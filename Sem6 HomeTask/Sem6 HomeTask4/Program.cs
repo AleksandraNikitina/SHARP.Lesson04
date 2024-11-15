02ca53ddf756670e24ce5f5604182376d0c5bbc1
Aleksandra <asandrabelka@gmail,com>
2024-11-15 03:03:02 +0200

    Make local function

--- Sem6 HomeTask/Sem6 HomeTask4/Program.cs
+++ Sem6 HomeTask/Sem6 HomeTask4/Program.cs
@@ -3,12 +3,19 @@
 // В полученной строке слова должны быть также разделены пробелами.
 
 
-string strNew = "Hello my world!";
-string[] strNewSplit = strNew.Split(); // ["Hello", "my", "world"]
-string res = "";
-for (int i = strNewSplit.Length - 1; i >= 0; i--)
+static string ReverseWords(string strNew)
 {
-    res += strNewSplit[i] + " ";
+    string[] strNewSplit = strNew.Split(); // ["Hello", "my", "world"]
+    string res = "";
+    for (int i = strNewSplit.Length - 1; i >= 0; i--)
+    {
+        res += strNewSplit[i] + " ";
+    }
+    return res;
 }
+
+string strNew = "Hello my world!";
+
+string res = ReverseWords(strNew);
 
 Console.WriteLine(res);
